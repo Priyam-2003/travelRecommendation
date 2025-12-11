4340f093f33d4c0f29eb650926d2c126b8c1102f
PRIYAM BAG <priyambag76@gmail.com>
2025-12-11 22:44:58 +0530

    After the contract

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,8 +1,11 @@
 const navLinks = document.querySelectorAll('.nav-links li');
 const sections = document.querySelectorAll('section .Home, section .About, section .Contact');
+const search = document.querySelector(".search-bar");
+
 sections.forEach(sec => {
-    if(sec.classList.contains('Contact')) {
+    if(sec.classList.contains('Home')) {
         sec.classList.remove('deactive');
+        search.classList.remove('deactive');
     } else {
         sec.classList.add('deactive');
     }
@@ -11,6 +14,12 @@ navLinks.forEach(link => {
     link.addEventListener('click', () => {
         const targetId = link.id;
         sections.forEach(sec => {
+            if(targetId === 'Home'){
+                search.classList.remove('deactive');
+            }
+            else{
+                search.classList.add('deactive');
+            }
             if(sec.classList.contains(targetId)) {
                 sec.classList.remove('deactive');
             } else {
